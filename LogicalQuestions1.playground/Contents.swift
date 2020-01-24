09839fa27dacd1285c4318f0d00904e361873689
Shubham Kapoor <Shubham.Kapoor@arris.com>
2020-01-24 11:38:26 +0530

    Question 5. Get Tallest Candle count

--- LogicalQuestions1.playground/Contents.swift
+++ LogicalQuestions1.playground/Contents.swift
@@ -104,3 +104,20 @@ func findMinMax(numberArray: [Int]) {
 }
 
 findMinMax(numberArray: [1,2,3,4,5])
+
+
+/*-----------------------------------------------------NextQuestion-----------------------------------------------------*/
+
+/* 5.You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow out.
+ 
+    For example, if your niece is turning 4 years old, and the cake will have 4 candles of height 4, 4, 1, 3, she will be able to blow out 2 candles successfully, since the tallest candles are of height 4 and there are 4 such candles.
+ */
+
+func getCandlesBlownOut(candles: [Int]) -> Int {
+    
+    let candlesInOrder = candles.sorted()
+    let tallestCandleHeight = candlesInOrder.last
+    return candlesInOrder.filter{$0 == tallestCandleHeight}.count
+}
+
+getCandlesBlownOut(candles: [4, 4, 1, 3])
